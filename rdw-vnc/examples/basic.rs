e52b66d995954805c343a9f091eb8b77ea05bb96
Marc-André Lureau <marcandre.lureau@redhat.com>
2021-04-07 16:16:49 +0400

    vnc: pimp a bit example
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- rdw-vnc/examples/basic.rs
+++ rdw-vnc/examples/basic.rs
@@ -1,26 +1,89 @@
-use gtk::prelude::*;
+use std::{cell::RefCell, sync::Arc};
 
-fn build_ui(application: &gtk::Application) {
-    let window = gtk::ApplicationWindow::new(application);
-
-    window.set_title(Some("Rdw-Vnc basic test"));
-    window.set_default_size(1024, 768);
-
-    let display = rdw_vnc::DisplayVnc::new();
-    display.connection().open_host("localhost", "5900").unwrap();
-    window.set_child(Some(&display));
-
-    window.show();
-}
+use gio::ApplicationFlags;
+use gtk::{gio, glib, prelude::*};
 
 fn main() {
     env_logger::init();
-    gvnc::set_debug(true);
 
-    let application = gtk::Application::new(Some("org.gnome.rdw-vnc.basic"), Default::default())
-        .expect("Initialization failed...");
+    let app = gtk::Application::new(
+        Some("org.gnome.rdw-vnc.basic"),
+        ApplicationFlags::NON_UNIQUE | ApplicationFlags::HANDLES_COMMAND_LINE,
+    )
+    .expect("Initialization failed...");
+    app.add_main_option(
+        "version",
+        glib::Char(0),
+        glib::OptionFlags::NONE,
+        glib::OptionArg::None,
+        "Show program version",
+        None,
+    );
+    app.add_main_option(
+        "debug",
+        glib::Char(0),
+        glib::OptionFlags::NONE,
+        glib::OptionArg::None,
+        "Enable gtk-vnc debugging",
+        None,
+    );
+    app.add_main_option(
+        &glib::OPTION_REMAINING,
+        glib::Char(0),
+        glib::OptionFlags::NONE,
+        glib::OptionArg::StringArray,
+        "URI",
+        Some("URI"),
+    );
+    app.connect_handle_local_options(|_, opt| {
+        if opt.lookup_value("version", None).is_some() {
+            println!("Version: {}", env!("CARGO_PKG_VERSION"));
+            return 0;
+        }
+        if opt.lookup_value("debug", None).is_some() {
+            gvnc::set_debug(true);
+        }
+        -1
+    });
 
-    application.connect_activate(build_ui);
+    let addr = Arc::new(RefCell::new(("localhost".to_string(), 5900)));
 
-    application.run();
+    let addr2 = addr.clone();
+    app.connect_command_line(move |app, cl| {
+        match cl
+            .get_options_dict()
+            .lookup_value(&glib::OPTION_REMAINING, None)
+        {
+            Some(args) => {
+                let mut arg = args.get_child_value(0).get::<String>().unwrap();
+                if !arg.starts_with("vnc://") {
+                    arg = format!("vnc://{}", arg);
+                }
+                let uri = glib::Uri::parse(&arg, glib::UriFlags::NONE).unwrap();
+                *addr2.borrow_mut() = (uri.get_host().unwrap().to_string(), uri.get_port());
+                app.activate();
+                -1
+            }
+            None => 1,
+        }
+    });
+
+    app.connect_activate(move |app| {
+        let window = gtk::ApplicationWindow::new(app);
+
+        window.set_title(Some("rdw-vnc example"));
+        window.set_default_size(1024, 768);
+
+        let display = rdw_vnc::DisplayVnc::new();
+        let (host, port) = &*addr.borrow();
+        display
+            .connection()
+            .open_host(&host, &format!("{}", port))
+            .unwrap();
+        window.set_child(Some(&display));
+
+        window.show();
+    });
+
+    app.run();
 }
